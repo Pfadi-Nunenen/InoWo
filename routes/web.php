66935cf737e275e82bf87830bac24d66b4a5c052
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-03-22 20:08:49 +0100

    Setup basic routing

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/overwatch', 301);
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => ['auth']], function () {
+    Route::any('/overwatch', 'OverwatchController@index')->name('overwatch');
+
+    Route::any('/users', 'UsersController@index')->name('users');
+    Route::get('/users/add', 'UsersController@create')->name('add-users');
+    Route::post('/users/store', 'UsersController@store')->name('store-users');
+    Route::get('/users/edit/{uid}', 'UsersController@edit')->name('edit-users');
+    Route::post('/users/update/{uid}', 'UsersController@update')->name('update-users');
+    Route::get('/users/destroy/{uid}', 'UsersController@destroy')->name('destroy-users');
+
+    Route::any('/transactions', 'PointTransactionController@index')->name('transactions');
+    Route::get('/transactions/add', 'PointTransactionController@create')->name('add-transactions');
+    Route::post('/transactions/store', 'PointTransactionController@store')->name('store-transactions');
+    Route::get('/transactions/edit/{trid}', 'PointTransactionController@edit')->name('edit-transactions');
+    Route::post('/transactions/update/{trid}', 'PointTransactionController@update')->name('update-transactions');
+    Route::get('/transactions/destroy/{trid}', 'PointTransactionController@destroy')->name('destroy-transactions');
+
+    Route::any('/credit', 'EmergencyController@index')->name('credit');
+    Route::get('/credit/add', 'EmergencyController@create')->name('add-numbers');
+    Route::post('/credit/store', 'EmergencyController@store')->name('store-numbers');
+    Route::get('/credit/edit/{nid}', 'EmergencyController@edit')->name('edit-numbers');
+    Route::post('/credit/update/{nid}', 'EmergencyController@update')->name('update-numbers');
+    Route::get('/credit/destroy/{nid}', 'EmergencyController@destroy')->name('destroy-numbers');
+});
