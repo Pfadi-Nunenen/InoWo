155cbc21f297f686ad3882c4adcab5eb0d1d58c9
Vento <vento@pfadi-nuenenen.ch>
2022-03-05 15:56:28 +0100

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,11 +1,10 @@
 window._ = require('lodash');
-window.Popper = require('popper.js').default;
 
 try {
-    window.$ = window.jQuery = require('jquery');
-
     require('bootstrap');
-} catch (e) { }
+} catch (e) {
+    console.error("Error: " + e);
+}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -16,20 +15,3 @@ try {
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-let token = document.head.querySelector('meta[name="csrf-token"]');
-
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
-
-require('bootstrap-select');
-require('bootstrap4-toggle');
