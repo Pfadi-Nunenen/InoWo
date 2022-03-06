3ed3053b5820a6bf9e65a57549557335d4067fd8
Vento <vento@pfadi-nuenenen.ch>
2022-03-06 20:48:08 +0100

    Create UsersTableSeeder.php

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Seed test admin
+        $seededAdminEmail = 'admin@inowo.ch';
+        $role = Role::where('name','=','Super-Admin')->first();
+        $user = User::where('email', '=', $seededAdminEmail)->first();
+        if ($user === null){
+            $user = User::create([
+                'scout_name' => 'Admin',
+                'first_name' => 'Admin',
+                'last_name' => 'Admin',
+                'email' => $seededAdminEmail,
+                'password' => Hash::make('password'),
+            ]);
+            $user->assignRole($role);
+            $user->save();
+        }
+    }
+}
