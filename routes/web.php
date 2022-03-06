2c382e7bed3cf9e294eb628f9436dfeb659c38a9
Vento <vento@pfadi-nuenenen.ch>
2022-03-06 16:31:25 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\OverwatchController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,27 +19,13 @@ Route::redirect('/', '/overwatch', 301);
 
 Auth::routes();
 
-Route::group(['middleware' => ['auth']], function () {
-    Route::any('/overwatch', 'OverwatchController@index')->name('overwatch');
+Route::group(['middleware' => ['auth']], function (){
+    Route::any('/overwatch', [OverwatchController::class, 'index'])->name('overwatch');
 
-    Route::any('/users', 'UsersController@index')->name('users');
-    Route::get('/users/add', 'UsersController@create')->name('add-users');
-    Route::post('/users/store', 'UsersController@store')->name('store-users');
-    Route::get('/users/edit/{uid}', 'UsersController@edit')->name('edit-users');
-    Route::post('/users/update/{uid}', 'UsersController@update')->name('update-users');
-    Route::get('/users/destroy/{uid}', 'UsersController@destroy')->name('destroy-users');
-
-    Route::any('/transactions', 'PointTransactionController@index')->name('transactions');
-    Route::get('/transactions/add', 'PointTransactionController@create')->name('add-transactions');
-    Route::post('/transactions/store', 'PointTransactionController@store')->name('store-transactions');
-    Route::get('/transactions/edit/{trid}', 'PointTransactionController@edit')->name('edit-transactions');
-    Route::post('/transactions/update/{trid}', 'PointTransactionController@update')->name('update-transactions');
-    Route::get('/transactions/destroy/{trid}', 'PointTransactionController@destroy')->name('destroy-transactions');
-
-    Route::any('/credit', 'EmergencyController@index')->name('credit');
-    Route::get('/credit/add', 'EmergencyController@create')->name('add-numbers');
-    Route::post('/credit/store', 'EmergencyController@store')->name('store-numbers');
-    Route::get('/credit/edit/{nid}', 'EmergencyController@edit')->name('edit-numbers');
-    Route::post('/credit/update/{nid}', 'EmergencyController@update')->name('update-numbers');
-    Route::get('/credit/destroy/{nid}', 'EmergencyController@destroy')->name('destroy-numbers');
+    Route::any('/users', [UsersController::class, 'index'])->name('users');
+    Route::get('/users/add', [UsersController::class, 'create'])->name('add-users');
+    Route::post('/users/store', [UsersController::class, 'store'])->name('store-users');
+    Route::get('/users/edit/{uid}', [UsersController::class, 'edit'])->name('edit-users');
+    Route::post('/users/update/{uid}', [UsersController::class, 'update'])->name('update-users');
+    Route::get('/users/destroy/{uid}', [UsersController::class, 'destroy'])->name('destroy-users');
 });
