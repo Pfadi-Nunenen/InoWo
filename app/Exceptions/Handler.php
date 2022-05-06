2647146fa52faf654010a5f5b7c9b937135ce623
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2022-05-06 17:59:19 +0200

    Update Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\App;
 use Throwable;
 
 class Handler extends ExceptionHandler
