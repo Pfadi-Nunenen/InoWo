dd43a412bf21658d8b55a0777afb98630e29deaa
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2022-05-06 18:26:10 +0200

    Update UserObserver.php

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -12,52 +12,8 @@ class UserObserver
      * @param  \App\Models\User  $user
      * @return void
      */
-    public function created(User $user)
+    function created(User $user)
     {
         $user->assignRole('user');
-    }
-
-    /**
-     * Handle the User "updated" event.
-     *
-     * @param  \App\Models\User  $user
-     * @return void
-     */
-    public function updated(User $user)
-    {
-        //
-    }
-
-    /**
-     * Handle the User "deleted" event.
-     *
-     * @param  \App\Models\User  $user
-     * @return void
-     */
-    public function deleted(User $user)
-    {
-        //
-    }
-
-    /**
-     * Handle the User "restored" event.
-     *
-     * @param  \App\Models\User  $user
-     * @return void
-     */
-    public function restored(User $user)
-    {
-        //
-    }
-
-    /**
-     * Handle the User "force deleted" event.
-     *
-     * @param  \App\Models\User  $user
-     * @return void
-     */
-    public function forceDeleted(User $user)
-    {
-        //
     }
 }
