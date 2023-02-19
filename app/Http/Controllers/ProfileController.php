610d8ea426834ea3fc8d09fc2f762a970e9c416f
Vento <github@brenneisen.me>
2023-02-19 21:44:14 +0100

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,7 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Settings;
+use App\Models\User;
+use Carbon\CarbonPeriod;
+use Illuminate\Support\Facades\Auth;
+
 class ProfileController extends Controller
 {
-    //
+    public function index()
+    {
+        $uid = Auth::id();
+        $user = User::find($uid)->first();
+
+        $settings = Settings::first();
+        $period = CarbonPeriod::create($settings->start_date, $settings->end_date);
+
+        return view('profile.profile', ['user' => $user, 'period' => $period]);
+    }
+
+    public function update()
+    {
+
+    }
 }
