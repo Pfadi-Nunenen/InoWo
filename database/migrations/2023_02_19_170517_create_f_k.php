a45bdaf625abff8d07d728dac12e8221e84c7549
Vento <github@brenneisen.me>
2023-02-19 18:25:59 +0100

    Update 2023_02_19_170517_create_f_k.php

--- database/migrations/2023_02_19_170517_create_f_k.php
+++ database/migrations/2023_02_19_170517_create_f_k.php
@@ -11,31 +11,31 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function(Blueprint $table){
+        Schema::table('users', function (Blueprint $table) {
             $table->foreign('fk_sleep_temp')->references('id')->on('sleeptemp')->onDelete('cascade');
         });
 
-        Schema::table('users', function(Blueprint $table){
+        Schema::table('users', function (Blueprint $table) {
             $table->foreign('fk_job')->references('id')->on('jobs')->onDelete('cascade');
         });
 
-        Schema::table('bills', function(Blueprint $table){
+        Schema::table('bills', function (Blueprint $table) {
             $table->foreign('fk_user')->references('id')->on('users')->onDelete('cascade');
         });
 
-        Schema::table('bills', function(Blueprint $table){
+        Schema::table('bills', function (Blueprint $table) {
             $table->foreign('fk_billtype')->references('id')->on('billtype')->onDelete('cascade');
         });
 
-        Schema::table('meals', function(Blueprint $table){
+        Schema::table('meals', function (Blueprint $table) {
             $table->foreign('fk_user')->references('id')->on('users')->onDelete('cascade');
         });
 
-        Schema::table('meals', function(Blueprint $table){
+        Schema::table('meals', function (Blueprint $table) {
             $table->foreign('fk_mealtypes')->references('id')->on('mealtypes')->onDelete('cascade');
         });
 
-        Schema::table('mealtypes', function(Blueprint $table){
+        Schema::table('mealtypes', function (Blueprint $table) {
             $table->foreign('fk_cooktime')->references('id')->on('cooktimes')->onDelete('cascade');
         });
     }
