95da9bd833a1a64ab080da5c0eee7ada719b7268
Vento <github@brenneisen.me>
2023-02-19 21:45:07 +0100

    Update nav.blade.php

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,65 +1,62 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-laravel">
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('app.name', 'Laravel') }}
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
-            <span class="sr-only">{!! trans('titles.toggleNav') !!}</span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        @if(!Auth::guest())
-            <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+            @if(!Auth::guest())
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link {{ Request::is('overwatch/')}}" href="{{ url('/overwatch') }}">
+                        <a class="nav-link {{ Request::is('overwatch/') }}" href="{{ url('/overwatch') }}">
                             Overwatch
                         </a>
                     </li>
 
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#"  role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Administration
                         </a>
 
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li>
-                                <a class="dropdown-item {{ Request::is('users/')}}" href="{{ url('/users') }}">
-                                    Benutzer
-                                </a>
-                            </li>
-                            <li><hr class="dropdown-divider" /></li>
-                        </ul>
+                        <div class="dropdown-menu dropdown-menu-start" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('users') }}">
+                                Benutzer
+                            </a>
+                        </div>
+
                     </li>
                 </ul>
-        @endif
+            @endif
 
-        <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ms-auto">
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <!-- Authentication Links -->
                 @guest
-                    @if (Route::has('login'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                    @endif
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
 
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
                 @else
                     <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->scout_name }} <span class="caret"></span>
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('profile') }}">
+                                Profile
+                            </a>
+
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                                 document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
                             </a>
 
