783f875aa7e58318b292ba8a89bb1b44db05b409
Vento <github@brenneisen.me>
2023-02-19 17:31:17 +0100

    Update RolesTableSeeder.php

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -2,49 +2,45 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Role;
 use Spatie\Permission\PermissionRegistrar;
 
 class RolesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
         // Reset cached roles and permissions
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
         // Seed "Administrator" role
-        $seededRoleName = "Administator";
+        $seededRoleName = 'Administator';
         $role = Role::where('name', '=', $seededRoleName)->first();
-        if ($role === null){
+        if ($role === null) {
             $role = Role::create([
-                'name' => $seededRoleName
+                'name' => $seededRoleName,
             ]);
             $role->save();
         }
 
         // Seed "Buchhaltung" role
-        $seededRoleName = "Buchhaltung";
+        $seededRoleName = 'Buchhaltung';
         $role = Role::where('name', '=', $seededRoleName)->first();
-        if ($role === null){
+        if ($role === null) {
             $role = Role::create([
-                'name' => $seededRoleName
+                'name' => $seededRoleName,
             ]);
             $role->save();
         }
 
         // Seed "User" role
-        $seededRoleName = "User";
+        $seededRoleName = 'User';
         $role = Role::where('name', '=', $seededRoleName)->first();
-        if ($role === null){
+        if ($role === null) {
             $role = Role::create([
-                'name' => $seededRoleName
+                'name' => $seededRoleName,
             ]);
             $role->save();
         }
