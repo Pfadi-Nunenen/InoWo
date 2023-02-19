30654e8b3cc60d69fa2fbc7eea219f26e4bd8cae
Vento <github@brenneisen.me>
2023-02-19 18:10:02 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,18 +11,18 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com" />
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" />
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet" />
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-<div id="app">
-    @include('layouts.nav')
-
-    <main class="py-4">
-        @yield('content')
-    </main>
-</div>
+    <div id="app">
+        @include('layouts.nav')
+        
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
 </body>
 </html>
