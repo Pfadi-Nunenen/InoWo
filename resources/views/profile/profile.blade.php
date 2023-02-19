07e62662f6fa59a483836f47352766b9dccb544d
Vento <github@brenneisen.me>
2023-02-19 21:43:26 +0100

    Create profile.blade.php

---
+++ resources/views/profile/profile.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        @if(session()->has('message'))
+            <div class="alert alert-success">
+                {{ session()->get('message') }}
+            </div>
+        @endif
+
+        <div class="card">
+            <div class="card-header">
+                <h5 class="float-start">Profil</h5>
+
+                <a href="{{  route('overwatch') }}" class="float-end">Zurück zu Overwatch</a>
+            </div>
+            <div class="card-body">
+                Pfadiname: {{ $user->scout_name ?? '' }} <br />
+                Name: {{ $user->first_name }} {{ $user->last_name }} <br />
+                E-Mail: {{ $user->email }}
+            </div>
+        </div>
+
+        <div class="clearfix p-3"></div>
+
+        <div class="card">
+            <div class="card-header">
+                <h5 class="float-start">Anwesenheit & Essen</h5>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <tr>
+                            <th>Mahlzeit</th>
+                            @foreach($period as $date)
+                                <th>{{ $date->format('d.m.Y') }} ({{ $date->locale('de')->dayName }})</th>
+                            @endforeach
+                        </tr>
+                        <tr>
+                            <th>Z'Morge</th>
+                        </tr>
+                        <tr>
+                            <th>Z'Mittag</th>
+                        </tr>
+                        <tr>
+                            <th>Z'Nacht</th>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <div class="clearfix p-3"></div>
+
+        <div class="card">
+            <div class="card-header">
+                <h5 class="float-start">Belege & Einkäufe</h5>
+            </div>
+            <div class="card-body">
+
+            </div>
+        </div>
+    </div>
+@endsection
