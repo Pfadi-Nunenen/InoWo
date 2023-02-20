25598501f49aab937f093a6f8601d7c9b9ed9bf1
Vento <github@brenneisen.me>
2023-02-20 22:01:56 +0100

    Create presence.blade.php

---
+++ resources/views/profile/presence.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        @if(session()->has('message'))
+            <div class="alert alert-success">
+                {{ session()->get('message') }}
+            </div>
+        @endif
+
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="float-start">Anwesenheit & Essen</h5>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                            <tr>
+                                <th>Mahlzeit</th>
+                                <th>Z'Morge</th>
+                                <th>Z'Mittag</th>
+                                <th>Z'Nacht</th>
+                                <th>Mitnäh</th>
+                            </tr>
+                            {{ $meals }}
+
+                            @foreach($period as $date)
+                                <tr>
+                                    <td>{{ $date->format('d.m.Y') }} ({{ $date->locale('de')->dayName }})</td>
+                                    <td>{{ true }}</td>
+                                    <td>{{ true }}</td>
+                                    <td>{{ true }}</td>
+                                    <td>{{ true }}</td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                    <div class="input-group" id="adv-search">
+                        <button onclick="location.href='{{ route('add-profile-presence') }}'" type="button" class="btn btn-primary form-control mt-2">Anwesenheit Erfassen</button>
+                    </div>
+                </div>
+            </div>
+    </div>
+@endsection
