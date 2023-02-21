42b1e9ae297d02e1d582dd83a0fd09d67bbe9201
Vento <github@brenneisen.me>
2023-02-21 22:04:29 +0100

    Update 2023_02_19_170238_create_meal_types_table.php

--- database/migrations/2023_02_19_170238_create_meal_types_table.php
+++ database/migrations/2023_02_19_170238_create_meal_types_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('mealtypes', function (Blueprint $table) {
+        Schema::create('meal_types', function (Blueprint $table) {
             $table->id();
-            $table->string('mealtype_name')->unique();
+            $table->string('meal_types_name')->unique();
             $table->timestamps();
         });
     }
