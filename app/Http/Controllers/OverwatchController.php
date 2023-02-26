7ed99f17d32b326637e964c0b81c50c242fbabe2
Vento <github@brenneisen.me>
2023-02-26 22:19:27 +0100

    Update OverwatchController.php

--- app/Http/Controllers/OverwatchController.php
+++ app/Http/Controllers/OverwatchController.php
@@ -2,10 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Meal;
+use App\Models\MealType;
+use App\Models\Settings;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
+
 class OverwatchController extends Controller
 {
     public function index()
     {
-        return view('overwatch.overwatch');
+        $settings = Settings::first();
+        $period = CarbonPeriod::create($settings->start_date, $settings->end_date);
+
+        $meals = Meal::all();
+        $mealTypes = MealType::all();
+
+        $facturedMeals = [];
+        foreach ($period as $date) {
+            foreach ($mealTypes as $mealType) {
+                $facturedMeals[$date->format('d.m.Y')][$mealType['id']] = 0;
+            }
+        }
+
+        foreach($meals as $meal) {
+            foreach($facturedMeals as $date => $mealTypes) {
+                $mealDate = Carbon::parse($meal['meal_date'])->format('d.m.Y');
+
+                if($date == $mealDate){
+                    $facturedMeals[$mealDate][$meal['fk_meal_types']] += 1;
+                }
+            }
+        }
+
+        return view('overwatch.overwatch', ['meals' => $facturedMeals, 'rawMeals' => $meals]);
     }
 }
